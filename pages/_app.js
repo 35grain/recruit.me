84e427d41e1c138b5242e6825b48098197883f3a
35grain <richardaljasteabramson@gmail.com>
2023-02-01 12:33:09 +0200

    Init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    // 2. Use at the root of your app
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
